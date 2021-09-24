0f770f2d8f9731979f43019f21136c371b82ef6a
ang-jason <angjason896@gmail.com>
2021-09-24 17:14:03 +0800

    #10 add props and scaffolding

---
+++ src/RecipeList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+function RecipeList( {recipes} ) {
+    return (
+        <div>
+            {recipes.map(recipe => {
+                return (<Recipe 
+                key={recipe.id} 
+                {...recipe} 
+
+                    />)
+            })
+            }
+        </div>
+        
+    )
+}
+
+export default RecipeList
+
